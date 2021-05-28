2349b20b9ab57fb7d8047481e2ff2f4b894b719a
jungkihun <khkh0130@gmail.com>
2021-05-29 03:50:17 +0900

    jkh 1.0.5

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql');
 //const dataset = require('./app.js');
-var argv_ip  = process.argv[2]
+//var argv_ip  = process.argv[2]
 var db_info = {
-    host: argv_ip != '192.168.219.102' ?  '180.83.98.144':argv_ip.toString(),
+    host: process.env.DB_IP,//argv_ip != '192.168.219.102' ?  '180.83.98.144':argv_ip.toString(),
     port: process.env.DB_PORT,    
     user: process.env.DB_ID,
     password: process.env.DB_PW,
