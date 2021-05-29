d11e52207575f7e67c41c35a258d39c9b939a477
jungkihun <khkh0130@gmail.com>
2021-05-30 00:48:08 +0900

    jkh 1.0.5

--- project_db.sql
+++ project_db.sql
@@ -1,6 +1,6 @@
-CREATE SCHEMA 'projectA' DEFAULT CHARCTER SET utf8;
+CREATE SCHEMA 'projecta' DEFAULT CHARCTER SET utf8;
 
-CREATE TABLE projectA.users(
+CREATE TABLE projecta.users(
     id INT NOT NULL AUTO_INCREMENT,
     userName VARCHAR(20) NOT NULL,
     email VARCHAR(255) NOT NULL,
@@ -8,18 +8,18 @@ CREATE TABLE projectA.users(
     PRIMARY KEY(id),
     UNIQUE KEY email_idx(email)
 )default character set utf8 collate utf8_general_ci;
-CREATE TABLE projectA.QnAdata(
+CREATE TABLE projecta.QnAdata(
     id INT NOT NULL AUTO_INCREMENT,
     userName VARCHAR(20) NOT NULL,
     title VARCHAR(300) NOT NULL,
     postData TEXT NOT NULL
 )default character set utf8 collate utf8_general_ci;
-CREATE TABLE projectA.logstack(
+CREATE TABLE projecta.logstack(
     id BIGINT NOT NULL AUTO_INCREMENT,
     errCord VARCHAR(30) NOT NULL,
     content VARCHAR(200) NOT NULL,
     errTime TIMESTAMP,
     PRIMARY KEY(id)
 )
-CREATE TABLE projectA.apiDataR()
-CREATE TABLE projectA.apiDataR()//요청
+CREATE TABLE projecta.apiDataR()
+CREATE TABLE projecta.apiDataR()//요청
