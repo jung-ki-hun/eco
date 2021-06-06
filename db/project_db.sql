2338534870b527186dfd0625a8f68590a94b469a
jungkihun <khkh0130@gmail.com>
2021-06-06 18:37:13 +0900

    jkh 1.0.7

---
+++ db/project_db.sql
@@ -0,0 +1,34 @@
+CREATE SCHEMA 'projecta' DEFAULT CHARCTER SET utf8;
+
+CREATE TABLE projecta.users(
+    id INT NOT NULL AUTO_INCREMENT,
+    userName VARCHAR(20) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    pw VARCHAR(255) NOT NULL,
+    PRIMARY KEY(id),
+    UNIQUE KEY email_idx(email)
+)default character set utf8 collate utf8_general_ci;
+CREATE TABLE projecta.QnAdata(
+    id INT NOT NULL AUTO_INCREMENT,
+    userName VARCHAR(20) NOT NULL,
+    title VARCHAR(300) NOT NULL,
+    postData TEXT NOT NULL,
+    PRIMARY KEY(id)
+)default character set utf8 collate utf8_general_ci;
+CREATE TABLE projecta.logstack(
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    errCord VARCHAR(30) NOT NULL,
+    content VARCHAR(200) NOT NULL,
+    errTime TIMESTAMP NOT NULL,
+    PRIMARY KEY(id)
+)default character set utf8 collate utf8_general_ci;
+CREATE TABLE projecta.users_community(
+    id INT NOT NULL AUTO_INCREMENT,
+    userName VARCHAR(20) NOT NULL,
+    friendName VARCHAR(20) NOT NULL,
+    community VARCHAR(5)NOT NULL,
+    PRIMARY KEY(id)
+)
+CREATE TABLE projecta.apiDataR()//요청
+
+CREATE TABLE projecta.apiDataR()//요청
