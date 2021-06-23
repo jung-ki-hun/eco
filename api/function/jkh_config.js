bc09be14c6a755a6dfdac53ef574f33ebf22d638
jungkihun <khkh0130@gmail.com>
2021-06-24 00:01:32 +0900

    jkh 1.0.7

--- api/function/jkh_config.js
+++ api/function/jkh_config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const config = {
  app:{
      host:process.env.T_HOST || '127.0.0.1',
-     port:process.env.T_PORT || '3001'
+     port:process.env.T_PORT || '3000'
  },
  db:{}
 }
