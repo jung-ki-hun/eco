4d657f2d1bac0d181004db58490a430221443589
jungkihun <khkh0130@gmail.com>
2021-06-25 23:16:28 +0900

    jkh 1.0.7

--- api/function/jkh_function.js
+++ api/function/jkh_function.js
@@ -17,10 +17,30 @@ var date_ymd = () => {
     return str;
 }
 
+/********************************
+ * ************암호화************
+*********************************/
+var crypto = require('crypto');
+var shasum = crypto.createHash('sha256');
+var key = '$!@T!EFQT%!#TWGW@T!#@%^';// 비밀키
+var cipher = (password)=>{
+    var cc = crypto.createCipher('aes192',key);
+    cc.update(password,'utf-8','base64');
+    var cipstr = cc.final('base64');
+    return cipstr;
+}//암호화 함수
+var dcipher = (password)=>{
+    var dc = crypto.createDecipheriv('aes192',key);
+    dc.update(password, 'base64','utf-8');
+    var dcipstr = dc.final('utf-8');
+    return dcipstr;
+}
 
 module.exports = {
     isEmpty,
     date_time,
     date_ymd,
+    cipher,
+    dcipher
 }
 // log save
