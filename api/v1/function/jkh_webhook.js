74867989934e04dd4a253cc9db71011ff1f65856
jungkihun <khkh0130@gmail.com>
2021-07-02 15:17:22 +0900

    jkh 1.0.7

---
+++ api/v1/function/jkh_webhook.js
@@ -0,0 +1,33 @@
+// 정규식 전용 클래스 작성
+// null 값 찾아보기
+const webhook = require('webhook-discord');
+var stringToUrl = process.env.WEB_HOOK +"";
+const Hook = new webhook.Webhook(stringToUrl+"");
+///url 삽입 문제 해결 필요
+var sendMessage = (type, msg) => {
+    const str = "server_bot";
+    switch (type) {
+        case 'info':
+            Hook.info(str, msg);
+            break;
+        case 'warn':
+            Hook.warn(str, msg);
+            break;
+        case 'err':
+            Hook.error(str,msg);
+            break;
+        case 'success':
+            Hook.success(str, msg);
+            break;
+        case 'etc':
+            const msg_text = new webhook.MessageBuilder().setText(msg_text);
+            Hook.send(msg_text);
+            break;
+        default:
+            console.error("do not send discord");
+            break;
+    }
+}//함수ㄴㅇㄹㄴㅁㅇㄹㄴㅇㄹ
+
+module.exports = sendMessage;
+
