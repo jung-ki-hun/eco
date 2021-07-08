6d7cbac0eca94e8d9ece36c64979a232453ea9f6
jungkihun <khkh0130@gmail.com>
2021-07-08 19:10:08 +0900

    jkh 1.0.7

--- api/v1/admin/index.js
+++ api/v1/admin/index.js
@@ -2,5 +2,7 @@ const express = require('express');
 var app = express.Router();
 app.group((router)=>{
 router.group('/chat',require('./chat.js'));
+router.group('/chat',require('./login.js'));
+router.group('/chat',require('./chat.js'));
 })
 module.exports = app;
