05a1b51ecd0fdfdfef78d174ada0fed6272c70a1
jungkihun <khkh0130@gmail.com>
2021-08-06 23:20:37 +0900

    jkh 1.3.3 postgresql end

--- db/project_db3.sql
+++ db/project_db3.sql
@@ -1,27 +1,27 @@
 --postgresql
-CREATE TABLE projecta.users(
+CREATE TABLE users(
     user_id SERIAL NOT NULL,
     userName VARCHAR(20) NOT NULL,
     email VARCHAR(255) NOT NULL,
     pw VARCHAR(255) NOT NULL,
-    UNIQUE KEY email_idx(email),
+    UNIQUE (email),
     PRIMARY KEY(user_id)
-)default character set utf8 collate utf8_general_ci; --사용자 정보 테이블
-CREATE TABLE projecta.users_level(
+); --사용자 정보 테이블
+CREATE TABLE users_level(
     level_id SERIAL NOT NULL,
-    user_id INT FOREIGN KEY REFERENCES NOT NULL,
+    user_id SERIAL REFERENCES users(user_id),
     PRIMARY KEY(level_id)
-)default character set utf8 collate utf8_general_ci;--권한 테이블 
-CREATE TABLE projecta.login_log(
+);--권한 테이블 
+CREATE TABLE login_log(
     ll_no SERIAL NOT NULL,
-    user_id INT FOREIGN KEY REFERENCES NOT NULL,
+    user_id SERIAL REFERENCES users(user_id),
     log_time TIMESTAMP,
     PRIMARY KEY(ll_no)
-)default character set utf8 collate utf8_general_ci;--로그인 기록
-CREATE TABLE projecta.logstack(
+);--로그인 기록
+CREATE TABLE logstack(
     log_id SERIAL NOT NULL,
     errCord VARCHAR(30) NOT NULL,
     content VARCHAR(200) NOT NULL,
     errTime TIMESTAMP NOT NULL,
-    PRIMARY KEY(log_id) 
-)default character set utf8 collate utf8_general_ci;--에러로그
+    PRIMARY KEY(log_id)
+);--에러로그
