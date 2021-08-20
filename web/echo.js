04af4c19aa842f6df166c7d303fe0875e236d4b6
김동훈 <112303dh@naver.com>
2021-08-20 18:38:02 +0900

    2021-08-20 18:37

--- web/echo.js
+++ web/echo.js
@@ -34,6 +34,7 @@ var server = http
 
   */
 
+  /*
 const express = require('express');
 const app = express();
 const PORT = 3000;
@@ -46,3 +47,22 @@ app.get("/",(req,res)=>{
 app.listen(PORT,()=>{
     console.log(`Listening on http://127.0.0.1:${PORT}`)
 })
+*/
+
+// node_modules 에 있는 express 관련 파일을 가져온다.
+var express = require('express')
+
+// express 는 함수이므로, 반환값을 변수에 저장한다.
+var app = express()
+
+// Express에서 정적 파일 제공
+app.use('/',express.static(__dirname));
+
+// 3000 포트로 서버 오픈
+app.listen(3000, function() {
+    console.log("start! express server on port http://127.0.0.1:3000")
+})
+
+/*app.get('/',(req,res)=>{
+  res.sendFile(__dirname+"/index.html");
+});*/
