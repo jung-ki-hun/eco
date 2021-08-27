18215f26f66d086258e4c18079b24b55afc84d07
jungkihun <khkh0130@gmail.com>
2021-08-27 16:03:38 +0900

    jkh test pg

--- api/etc/test.js
+++ api/etc/test.js
@@ -7,21 +7,23 @@ const { pool, Q } = require('../../db/psqldb');
 const dbtest = async () => {
 
     try {
+        const ad =  '1@1';
         const sql1 = Q`
             SELECT 
               u.username
-            FORM
+            FROM
               users u
             WHERE       
-             u.email = 1@1
+             u.email = ${ad};
             `;//
+        console.log(sql1);
         const query1 = await pool.query(sql1);//조회 알고리즘
         if (jkh.isEmpty(query1.rows)) {
             jkh.webhook('err', response.msg)//log 보내는 역활
             return 'empty';
         }
         else {
-            const s =query1.row;
+            const s = query1.row;
             console.log(s);
             console.log('ddd');
             return s;
--- db/psqldb.js
+++ db/psqldb.js
@@ -25,7 +25,7 @@
 // };
 const db_info = require("../api/v1/function/jkh_config.js")//설정관련 데이터
 const SQL = require("sql-template-strings");
-const { Pool, Client } = require('pg');
+const { Pool, Q } = require('pg');
 const pool = new Pool(db_info.config.pgdb);
 module.exports = {
     getConnection: function () {
@@ -33,7 +33,7 @@ module.exports = {
     },
     pool, //= pool_set.init(),
     Q(string, ...rest) {
-        return SQL(string, ...rest.map(((x) => (typeof x === object && x !== null ? JSON.stringify(x) : x))))
+        return SQL(string.slice(0), ...rest.map(((x) => (typeof x === 'object' && x !== null ? JSON.stringify(x) : x))));
     }///쿼리 만드는 거
 
 }
