fe147c866e4cc58d83f1b3ff3ce2933bc9281865
jungkihun <khkh0130@gmail.com>
2021-09-11 14:55:14 +0900

    jkh pg index_login test

--- api/etc/test.js
+++ api/etc/test.js
@@ -3,8 +3,8 @@
 // const h = 1.5;
 // console.log(pi*r*r*h);
 
-// const { pool, Q } = require('../../db/psqldb');
-// const jkh = require('../v1/function/jkh_function');
+ const { pool, Q } = require('../../db/psqldb');
+ const jkh_fun = require('../v1/function/jkh_function');
 // const dbtest = async () => {
 //     let id = '1@1',pw ='T1UTH4GycT4fS/zRUGddGQ=='
 //     try {
@@ -44,12 +44,60 @@
 // console.log(dbtest())
 //console.log(jkh.cipheriv(`1234`));//테스트해보기
 
-const a = { 
-    a1:1,
-    a2:2
-}
-const b = {
-    ...a,
-    bb:5
-}
-console.log(b.a1);
+// const a = { 
+//     a1:1,
+//     a2:2
+// }
+// const b = {
+//     ...a,
+//     bb:5
+// }
+// console.log(b.a1);
+
+const index = async (id, pw) => {
+    console.log(id, pw);
+    var pw_c = jkh_fun.cipheriv(pw);//암호화 진행 //iv 버전으로 수정 필수 !!!!
+    console.log(id, pw_c);
+    var user;
+    try {
+        const sql1 = Q`
+          SELECT 
+            u.username,
+            ul.level_u,
+            u.user_id
+          FROM
+            users u, users_level ul
+          WHERE        
+            ul.level_u in (select level_u from users_level ul2, users u2 WHERE u2.user_id = ul2.user_id)
+            AND
+            u.email = ${id}
+            AND
+            u.pw = ${pw_c}
+          `;//암호화 한 데이터(pw)를 기반으로 검색 진행
+        const query1 = await pool.query(sql1);//조회 알고리즘
+        console.log(`abc ${query1.rows[0]}`);
+        if (jkh_fun.isEmpty(query1.rows[0].username)) {
+            console.log('login fail');
+            jkh_fun.webhook('err', response.msg)//log 보내는 역활
+        }
+        else {
+            let user_id = query1.rows[0].user_id;//사용자 key 추출
+            user = query1.rows[0];
+            //res.cookie('auth', true);//쿠키생성 추후 수정예정
+            const sql2 = 
+            Q`insert into login_log(user_id,log_time) values (${user_id},${jkh_fun.date_time()})`;
+            const query2 = await pool.query(sql2);
+            if (query2.errors) {
+                console.log(query2.errors);
+                jkh_fun.webhook('err', 'login sql insert err(500)');
+            }
+        }
+
+        return user;
+    }
+    catch (err) {
+        console.error(err);
+        jkh_fun.webhook('err', 'login sql select err(500)')//log 보내는 역활
+    }
+}//login 
+index('1@1','1234');
