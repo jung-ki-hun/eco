a886dddf1b752131c82d5181543bf302d95b6866
jungkihun <khkh0130@gmail.com>
2021-09-18 20:35:50 +0900

    api v2 make

--- api/v1/function/jkh_passportU.js
+++ api/v1/function/jkh_passportU.js
@@ -102,7 +102,7 @@ passport.use(
         },
     ),
 );
-//jwtstrategy
+//jwtstrategy  //https://www.stackhoarder.com/2019/07/17/node-js-passport-js-jwt-token-%EC%9D%B4%EC%9A%A9%ED%95%B4-%EB%A1%9C%EA%B7%B8%EC%9D%B8-%EA%B5%AC%ED%98%84/
 
 // passport.use(
 //     'user.jwt',
