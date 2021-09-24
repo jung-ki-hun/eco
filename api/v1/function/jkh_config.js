7191e766efc65c395bfea052a500a6f277312683
jungkihun <khkh0130@gmail.com>
2021-09-24 21:25:52 +0900

    jkh config add kakao

--- api/v1/function/jkh_config.js
+++ api/v1/function/jkh_config.js
@@ -37,7 +37,11 @@ const config = {
         url: process.env.WEB_HOOK || "",
     },
     kakao:{
-        url: process.env.WEB_HOOK || "",
+        //url: process.env.WEB_HOOK || "",
+        admin: process.env.KAKAO_ADMIN || "",
+        appkey: process.env.KAKAO_APP || "",
+        apikey: process.env.KAKAO_API || "",
+        jskey: process.env.KAKAO_JS || ""
     }    
 }
 module.exports = config;
