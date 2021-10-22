a95539c7ee5fcb79476fb60a9ffb9fa1cfb0731f
jungkihun <khkh0130@gmail.com>
2021-10-22 21:17:55 +0900

    jkh web_server folder is not commit

--- web_api_server_config.js
+++ web_api_server_config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps:[{
         name:'app',
         script:'./app.js',
-        instances: 2,//코어 수 0은 제한 없음 
+        instances: 2,//코어 수 0은 제한 없음 ///추가설정 예정
         exec_mode:'cluster'
     }]
 }
