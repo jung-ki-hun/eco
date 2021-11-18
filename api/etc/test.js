ae80b346258ec1166bbcb2f66d07efe5efdbdb69
jungkihun <khkh0130@gmail.com>
2021-11-19 00:23:58 +0900

    jkh test

--- api/etc/test.js
+++ api/etc/test.js
@@ -54,51 +54,66 @@
 // }
 // console.log(b.a1);
 
-const index = async (id, pw) => {
-    console.log(id, pw);
-    var pw_c = jkh_fun.cipheriv(pw);//암호화 진행 //iv 버전으로 수정 필수 !!!!
-    console.log(id, pw_c);
-    var user;
-    try {
-        const sql1 = Q`
-          SELECT 
-            u.username,
-            ul.level_u,
-            u.user_id
-          FROM
-            users u, users_level ul
-          WHERE        
-            ul.level_u in (select level_u from users_level ul2, users u2 WHERE u2.user_id = ul2.user_id)
-            AND
-            u.email = ${id}
-            AND
-            u.pw = ${pw_c}
-          `;//암호화 한 데이터(pw)를 기반으로 검색 진행
-        const query1 = await pool.query(sql1);//조회 알고리즘
-        console.log(`abc ${query1.rows[0]}`);
-        if (jkh_fun.isEmpty(query1.rows[0].username)) {
-            console.log('login fail');
-            jkh_fun.webhook('err', response.msg)//log 보내는 역활
-            return null;
-        }
-        else {
-            let user_id = query1.rows[0].user_id;//사용자 key 추출
-            user = query1.rows[0];
-            //res.cookie('auth', true);//쿠키생성 추후 수정예정
-            const sql2 = 
-            Q`insert into login_log(user_id,log_time) values (${user_id},${jkh_fun.date_time()})`;
-            const query2 = await pool.query(sql2);
-            if (query2.errors) {
-                console.log(query2.errors);
-                jkh_fun.webhook('err', 'login sql insert err(500)');
-            }
-        }
+// const index = async (id, pw) => {
+//     console.log(id, pw);
+//     var pw_c = jkh_fun.cipheriv(pw);//암호화 진행 //iv 버전으로 수정 필수 !!!!
+//     console.log(id, pw_c);
+//     var user;
+//     try {
+//         const sql1 = Q`
+//           SELECT 
+//             u.username,
+//             ul.level_u,
+//             u.user_id
+//           FROM
+//             users u, users_level ul
+//           WHERE        
+//             ul.level_u in (select level_u from users_level ul2, users u2 WHERE u2.user_id = ul2.user_id)
+//             AND
+//             u.email = ${id}
+//             AND
+//             u.pw = ${pw_c}
+//           `;//암호화 한 데이터(pw)를 기반으로 검색 진행
+//         const query1 = await pool.query(sql1);//조회 알고리즘
+//         console.log(`abc ${query1.rows[0]}`);
+//         if (jkh_fun.isEmpty(query1.rows[0].username)) {
+//             console.log('login fail');
+//             jkh_fun.webhook('err', response.msg)//log 보내는 역활
+//             return null;
+//         }
+//         else {
+//             let user_id = query1.rows[0].user_id;//사용자 key 추출
+//             user = query1.rows[0];
+//             //res.cookie('auth', true);//쿠키생성 추후 수정예정
+//             const sql2 = 
+//             Q`insert into login_log(user_id,log_time) values (${user_id},${jkh_fun.date_time()})`;
+//             const query2 = await pool.query(sql2);
+//             if (query2.errors) {
+//                 console.log(query2.errors);
+//                 jkh_fun.webhook('err', 'login sql insert err(500)');
+//             }
+//         }
 
-        return user;
-    }
-    catch (err) {
-        console.error(err);
-        jkh_fun.webhook('err', 'login sql select err(500)')//log 보내는 역활
-    }
-}//login 
-console.log('sad'+index('1@1','1234'));
+//         return user;
+//     }
+//     catch (err) {
+//         console.error(err);
+//         jkh_fun.webhook('err', 'login sql select err(500)')//log 보내는 역활
+//     }
+// }//login 
+// console.log('sad'+index('1@1','1234'));
+const express = require('express');
+
+const app = express();
+const port = 3000;
+const path = require('path');
+var static = require('serve-static');
+app.use('/w', static(path.join(__dirname, 'public'))); 
+//----------------
+app.get('/', (req, res) => {
+  return res.redirect(302,'/w/index.html');//sendFile(`C:/Users/whdgu/Documents/study/public/index.html`);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
