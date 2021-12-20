8917f7be0c1f9b36342d3e6951b4f3fcbcca1951
jungkihun <khkh0130@gmail.com>
2021-12-20 21:21:34 +0900

    jkh edit login

--- api/v1/user/login.js
+++ api/v1/user/login.js
@@ -20,7 +20,7 @@ const passport = require('../function/jkh_passportU');
     return res.status(500).json(req.user);
   }
   // 로그인 성공 시
-  res.cookie(cookie_d,true);
+  res.cookie('key',cookie_d);
   //res.send(`token :   ${token}`); //토근 보내주는 구간
   return res.json({ token });
 }//login 
@@ -49,53 +49,7 @@ const del_log = async (req,res) =>{
     query: null, // 응답 값(JSON 형식) null, Object, Array, Boolean 중 하나
     msg: 'Successful',
   };
-  // const params = {
-  //   ...req.query,
-  //   ...req.params,
-  //   ...req.body,
-  //   id: req.uesr.email,
-  //   pw: req.user.password
-  // }
-  // var pw_c = jkh.cipher(params.pw);
-  //   try {
-  //   const sql1 = Q`
-  //       SELECT 
-  //         u.username,
-  //         ul.level_u
-  //       FORM
-  //         users u, users_level ul
-  //       WHERE        
-  //         ul.level_u in (select level_u form users_level ul2, users u2 WHERE u2.user_id = ul2.user_id)
-  //         AND
-  //         u.email = ${params.id}
-  //         AND
-  //         u.pw = ${params.pw}
-  //       `;//
-  //   const query1 = await pool.query(sql1);//조회 알고리즘
-  //   if (jkh.isEmpty(query1.rows)) {
-  //     response.state = 3;
-  //     response.msg = 'login failed';
-  //     jkh.webhook('err', response.msg)//log 보내는 역활
-  //     return res.state(404).send(json(response));
-  //   }
-  //   else {
-  //     const user_id = query1.rows[0].user_id;//사용자 key 추출
-  //     session.user = {
-  //       name: response.query,//results[0].user_name;//results[0];
-  //       password: req_data.pw,
-  //       email: req_data.email
-  //     }//새션생성
-  //     res.cookie('auth',true);//쿠키생성 추후 수정예정
-  //     response.state = 1; 
-  //     response.msg = 'login Success';
-  //     jkh.webhook('Success', response.msg)//log 보내는 역활 -> 디스코드
-  //   }
-  res.clearCookie('id');//수정필요
-  //}
-  // catch (err) {
-  //   console.error(err);
-  //   jkh.webhook.sendMessage('err','login sql select err(500)')//log 보내는 역활
-  // }
+  res.clearCookie('key');//수정필요
   return res.state(200).join(response);//데이터 전송 !!
 }
 
