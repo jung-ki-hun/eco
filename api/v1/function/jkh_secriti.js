9ddd63177f06db242893ef0d2c13ee19cc72acf1
jungkihun <khkh0130@gmail.com>
2022-02-21 06:42:06 +0900

    jkh file edit

--- api/v1/function/jkh_secriti.js
+++ api/v1/function/jkh_secriti.js
@@ -1,3 +1,11 @@
-const ip = ()=>{
+const jkh = require('./jkh_function');
+const ip_ban = (ip_client,)=>{
     const ip_address  = [];
+    const path = './';
+    const file_name = 'ip_ban_address';
+    ip_address= jkh.file_r(path,file_name).split('\n');
+
+}
+module.exports ={
+    ip_ban
 }
